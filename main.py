acb94e4a5092a5e16bce48db80bdd9054cdeb24b
Mike DeLong <mikedelong@outlook.com>
2023-05-05 11:08:11 -0400

    refactor: eliminate the named variable in the format since we only have one

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def main():
     soup = BeautifulSoup(html_content, 'html.parser')
     links = soup.findAll('a')
 
-    logger.info(msg='done; time: {x:.3f}'.format(x=(now() - time_start).seconds))
+    logger.info(msg='done; time: {:0.3f}'.format((now() - time_start).seconds))
 
 
 if __name__ == '__main__':
