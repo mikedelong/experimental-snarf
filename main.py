6dbd49901076f67ac1678d45c68ab1a8e972434d
Mike DeLong <mikedelong@outlook.com>
2023-05-05 11:04:05 -0400

    feature: create the downloads folder if it does not exist

--- main.py
+++ main.py
@@ -2,17 +2,22 @@
 Main with simple website scrape
 """
 
+from json import load
 from logging import INFO
 from logging import basicConfig
 from logging import getLogger
 
 from arrow import now
+from requests import get
+from bs4 import BeautifulSoup
 
-from json import load
+from os.path import exists
+from os import makedirs
 
 basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=INFO)
 logger = getLogger(__name__)
 
+DOWNLOADS_FOLDER = './downloads/'
 MODE_READ = 'r'
 SETTINGS_FILE = './main.json'
 
@@ -24,9 +29,19 @@ def main():
     with open(encoding='utf-8', file=SETTINGS_FILE, mode=MODE_READ, ) as input_fp:
         settings = load(fp=input_fp)
 
+    if not exists(DOWNLOADS_FOLDER):
+        logger.info(msg='creating output folder: {}'.format(DOWNLOADS_FOLDER),)
+        makedirs(name=DOWNLOADS_FOLDER, )
+
     url = settings['url']
     logger.info(msg='url: {url}'.format(url=url))
 
+    response = get(url=url)
+    html_content = response.text
+    soup = BeautifulSoup(html_content, 'html.parser')
+    links = soup.findAll('a')
+
+    # todo switch to msg= here
     logger.info('done; time: %0.2f', (now() - time_start).seconds)
 
 
