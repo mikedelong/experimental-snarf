7aa4accbdd47aa96513cd7289ca9998d22cf61d1
Mike DeLong <mikedelong@outlook.com>
2023-05-08 12:18:58 -0400

    feature: first version to use Selenium; still a work in progress

--- main.py
+++ main.py
@@ -6,20 +6,51 @@ from json import load
 from logging import INFO
 from logging import basicConfig
 from logging import getLogger
+from os import makedirs
+from os.path import basename
+from os.path import exists
 
 from arrow import now
-from requests import get
 from bs4 import BeautifulSoup
+from requests import get
 
-from os.path import exists
-from os import makedirs
+from selenium import webdriver
+
 
 basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=INFO)
 logger = getLogger(__name__)
 
+ARGUMENTS = [
+    '--disable-extensions',
+    '--disable-infobars',
+    '--disable-dev-shm-usage',
+    '--disable-browser-side-navigation',
+    '--ignore-certificate-errors',
+    '--no-sandbox',
+    '--remote-debugging-port=9222',
+    '--test-type'
+]
+BINARY_LOCATION = '/usr/bin/chromium-browser'
 DOWNLOADS_FOLDER = './downloads/'
+EXECUTABLE_PATH = '/usr/bin/chromedriver'
+HTML_FOLDER = './html/'
 MODE_READ = 'r'
+MODE_WRITE = 'w'
+MODE_WRITE_BINARY = 'wb'
 SETTINGS_FILE = './main.json'
+
+
+def images(url: str, folder: str):
+    response = get(url=url,)
+    soup = BeautifulSoup(markup=response.content, features='html.parser',)
+    tags = soup.findAll(name='img',)
+    for tag in tags:
+        image_url = tag.get('src')
+        image_response = get(url=image_url,)
+        image_filename = basename(image_url)
+        with open(file=folder + image_filename, mode=MODE_WRITE_BINARY) as output_fp:
+            output_fp.write(image_response.content)
+
 
 
 def main():
@@ -29,23 +60,34 @@ def main():
     with open(encoding='utf-8', file=SETTINGS_FILE, mode=MODE_READ, ) as input_fp:
         settings = load(fp=input_fp)
 
-    if not exists(DOWNLOADS_FOLDER):
-        logger.info(msg='creating output folder: {}'.format(DOWNLOADS_FOLDER), )
-        makedirs(name=DOWNLOADS_FOLDER, )
+    for folder in [DOWNLOADS_FOLDER, HTML_FOLDER]:
+        if not exists(folder):
+            logger.info(msg='creating folder: {}'.format(folder), )
+            makedirs(name=folder, )
 
     url = settings['url']
     logger.info(msg='url: {url}'.format(url=url))
 
-    response = get(url=url)
-    html_content = response.text
-    soup = BeautifulSoup(html_content, 'html.parser')
-    links = soup.findAll('a')
+    # let's get the content from the URL first
+    options = webdriver.ChromeOptions()
+    for argument in ARGUMENTS:
+        options.add_argument(argument=argument)
+    options.binary_location = BINARY_LOCATION
+    options.headless = True
+
+    driver = webdriver.Chrome(executable_path=EXECUTABLE_PATH, options=options)
+    driver.get(url=url,)
+    page_source = driver.page_source
+    logger.info(msg='page source length: {}'.format(len(page_source)))
+    return page_source
+
+    # images(url=url, folder=DOWNLOADS_FOLDER)
 
     logger.info(msg='done; time: {:0.3f}'.format((now() - time_start).seconds))
 
 
 if __name__ == '__main__':
     try:
-        main()
+        t = main()
     except Exception as exception:
         logger.error(exc_info=True, msg='error:')
